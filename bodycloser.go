992fe8e0d854ba89d36943f67d8a87afdc06b47f
Bjorn Borud <borud@lab5e.com>
2025-09-01 21:29:30 +0200

    also drain body before closing

--- bodycloser.go
+++ bodycloser.go
@@ -1,12 +1,14 @@
 package nethsm
 
 import (
+	"io"
 	"net/http"
 )
 
-// closeBody closes the resp.Body if non-nil
+// closeBody drains and closes the resp.Body if non-nil
 func closeBody(resp *http.Response) {
 	if resp != nil && resp.Body != nil {
+		_, _ = io.Copy(io.Discard, resp.Body)
 		resp.Body.Close()
 	}
 }
